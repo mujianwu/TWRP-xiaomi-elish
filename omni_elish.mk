aefeade21d52dec0758eb572e9d71f3a761fb4bb
mujianwu <62330315+mujianwu@users.noreply.github.com>
2021-11-02 16:52:05 +0800

    Update omni_elish.mk

--- omni_elish.mk
+++ omni_elish.mk
@@ -6,6 +6,7 @@
 #
 
 # Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product-if-exists, $(SRC_TARGET_DIR)/product/embedded.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
