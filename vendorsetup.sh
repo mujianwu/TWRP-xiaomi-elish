28919b438eb7b08cde9555702daaf04bb2faad33
mujianwu <62330315+mujianwu@users.noreply.github.com>
2021-11-02 22:17:47 +0800

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -5,6 +5,6 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-add_lunch_combo omni_elish-user
-add_lunch_combo omni_elish-userdebug
-add_lunch_combo omni_elish-eng
+#add_lunch_combo omni_elish-user
+#add_lunch_combo omni_elish-userdebug
+#add_lunch_combo omni_elish-eng
